d3753972bb3f5e994857f23bec6ac035aafcbca1
bhanuprasad142 <41734437+bhanuprasad142@users.noreply.github.com>
2018-07-31 15:03:12 +0530

    Create sn.java

---
+++ sn.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		int a,i,sum=0;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		for(i=0;i<=a;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
