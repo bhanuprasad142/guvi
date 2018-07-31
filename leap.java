0e64a92b6455c4ed80af292750867dd492cc6e76
bhanuprasad142 <41734437+bhanuprasad142@users.noreply.github.com>
2018-07-31 12:08:58 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		int year;
+	   Scanner s=new Scanner(System.in);
+	   year=s.nextInt();
+	   if(year%4==0)
+	   {
+	   	System.out.println("yes");
+	   }
+	   else
+	   {
+	   		   	System.out.println("no");
+	   }
+	}
+}
