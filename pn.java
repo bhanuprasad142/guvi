12a0fcdee62d9b0a3050d5feac63317d06a0f957
bhanuprasad142 <41734437+bhanuprasad142@users.noreply.github.com>
2018-07-31 15:49:52 +0530

    Create pn.java

---
+++ pn.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+       int n;
+       Scanner s=new Scanner(System.in);
+       n=s.nextInt();
+       if(n>0)
+       {
+       	System.out.println("Positive");
+       }
+       else
+       {
+       	System.out.println("Negative");
+       }
+       
+	}
+}
